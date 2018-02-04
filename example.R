93386b2843576642bc3b71ccf5cccabf872b7538
rhinophile <bijan.text@gmail.com>
2018-02-04 14:00:25 -0700

    Example

--- example.R
+++ example.R
@@ -23,3 +23,7 @@ colMeans(summ$ymax)
 colMeans(summ$betas)
 colMeans(summ$tau)
 colMeans(summ$sigma)
+
+boxplot(ssOut$chains[,c("beta.1", "beta.2")], outline = F)
+boxplot(ssOut$chains[,c("ymax.1", "ymax.2", "ymax.3", "ymax.4")], outline = F)
+boxplot(ssOut$chains[,c("sigma", "tau")], outline = F)
