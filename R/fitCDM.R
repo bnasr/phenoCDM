63957de9408bce2010e2308815cd978e0cb29941
rhinophile <bijan.text@gmail.com>
2018-02-05 11:00:53 -0700

    connectivity

--- R/fitCDM.R
+++ R/fitCDM.R
@@ -3,13 +3,13 @@
 #' This function fits a CDM model on the input data as it is described by the phenoSim function.
 #' @param x Matrix of predictors [N x p].
 #' @param z Vector of reponse values [N x 1].
-#' @param connect Connectivity matrix [N x 2].
+#' @param connect The connectivity matrix for the z vector [n x 2]. Each row contains the last and next elements of the time-series. NA values means not connected.
 #' @param nGibbs Number of Gibbs itterations
 #' @param nBurnin Number of burn-in itterations.
 #' @param n.adapt Number of itterations for adaptive sampling
 #' @param n.chains Number of Gibbs sampling chains
 #' @param quiet whether to report the progress
-#' @param calcLatentGibbs Whether to calculate the latent states 
+#' @param calcLatentGibbs Whether to calculate the latent states
 #' @keywords  CDM Model Fit
 #' @export
 #' @examples
@@ -25,16 +25,16 @@
 #'                   miss = 0.1, #fraction of missing data
 #'                   ymax = c(9,5,7, 3) #maximum of saturation trajectory
 #' )
-#' 
-#' ssOut <- fitCDM(x = ssSim$x, #predictors  
+#'
+#' ssOut <- fitCDM(x = ssSim$x, #predictors
 #'                 nGibbs = 2000,
 #'                 nBurnin = 1000,
 #'                 z = ssSim$z,#response
 #'                 connect = ssSim$connect, #connectivity of time data
 #'                 quiet=T)
-#' 
+#'
 #' summ <- getGibbsSummary(ssOut, burnin = 1000, sigmaPerSeason = F)
-#' 
+#'
 #' colMeans(summ$ymax)
 #' colMeans(summ$betas)
 #' colMeans(summ$tau)
@@ -43,15 +43,15 @@
 fitCDM <- function(x, z, connect=NULL,
                    # HeadTail =NULL,
                    nGibbs=1000,
-                   nBurnin = 1, 
+                   nBurnin = 1,
                    n.adapt=100,
-                   n.chains=4, 
+                   n.chains=4,
                    quiet=F,
                    calcLatentGibbs=F
-                   
+
 ){
   require(rjags)
-  
+
   if(!is.null(connect)){
     Head <- which(is.na(connect[,1]))
     Tail <- which(is.na(connect[,2]))
@@ -64,49 +64,49 @@ fitCDM <- function(x, z, connect=NULL,
   {
     stop('One of connect or HeadTail arguments should be defined.')
   }
-  
-  model <- textConnection('model 
+
+  model <- textConnection('model
                           {
-                          for (i in 1:N) 
+                          for (i in 1:N)
                           {
                           z[i] ~ dnorm(y[i], tau2)
                           }
-                          
+
                           tau2 <- pow(tau, -2)
                           tau ~ dunif(0, 100)
-                          
-                          for (i in connectHead) 
+
+                          for (i in connectHead)
                           {
                           y[i] ~ dnorm(0, sigma2)
                           }
-                          
-                          for (i in connectBody) 
+
+                          for (i in connectBody)
                           {
                           dy[i] ~ dnorm(x[i-1,]%*%beta*(1-y[i-1]/ymax[blocks[i]]), sigma2)
                           y[i] <- y[i-1] + max(0, dy[i])
                           }
-                          
-                          for (i in connectTail) 
+
+                          for (i in connectTail)
                           {
                           dy[i] ~ dnorm(x[i-1,]%*%beta*(1 - y[i-1]/ymax[blocks[i]]), sigma2)
                           y[i] <- y[i-1] + max(0, dy[i])
                           }
-                          
+
                           for (i in 1:nblocks)
                           {
                           ymax[i] ~ dnorm(0, .001)T(0,10000)
                           }
-                          for (i in 1:p) 
+                          for (i in 1:p)
                           {
                           beta[i] ~ dnorm(0, .0001)
                           }
-                          
+
                           sigma2 <- pow(sigma, -2)
                           sigma ~ dunif(0.01, 0.03)
                           }')
-  
-  ssModel <- jags.model(model, 
-                        quiet = quiet, 
+
+  ssModel <- jags.model(model,
+                        quiet = quiet,
                         data = list('x' = x,
                                     'z' = z,
                                     'N' = nrow(x),
@@ -118,15 +118,15 @@ fitCDM <- function(x, z, connect=NULL,
                                     connectTail=Tail),
                         n.chains = n.chains,
                         n.adapt = n.adapt)
-  
+
   update(ssModel, nBurnin)
-  
+
   ssSamples <- jags.samples(ssModel,c('y','beta', 'sigma', 'tau', 'ymax'), nGibbs )
-  
-  
+
+
   # print(ssSamples)
-  
-  
+
+
   ssGibbs.jags <- data.frame(beta=t(apply(ssSamples$beta, c(1,2), mean)),
                              ymax=t(apply(ssSamples$ymax, c(1,2), mean)),
                              sigma=t(apply(ssSamples$sigma, c(1,2), mean)),
@@ -136,19 +136,19 @@ fitCDM <- function(x, z, connect=NULL,
   if(calcLatentGibbs) latentGibbs <- t(apply(ssSamples$y, c(1,2), mean))
   ww <- grep(pattern = 'beta', colnames(ssGibbs.jags))
   if(!is.null(colnames(x))) colnames(ssGibbs.jags)[ww] <- colnames(x)
-  return(list(model=ssModel, 
-              chains=ssGibbs.jags, 
-              nBurnin= nBurnin, 
+  return(list(model=ssModel,
+              chains=ssGibbs.jags,
+              nBurnin= nBurnin,
               nGibbs=nGibbs,
-              latentGibbs = latentGibbs, 
+              latentGibbs = latentGibbs,
               rawsamples = ssSamples,
-              data =list(x = x, 
-                         z = z, 
+              data =list(x = x,
+                         z = z,
                          connect = connect,
                          # HeadTail = HeadTail,
-                         nBurnin = nBurnin, 
+                         nBurnin = nBurnin,
                          nGibbs = nGibbs,
-                         n.chains=n.chains, 
+                         n.chains=n.chains,
                          n.adapt = n.adapt)))
 }
 
--- R/phenoSimPlot.R
+++ R/phenoSimPlot.R
@@ -2,9 +2,9 @@
 #'
 #' This function plots the time-series data described with a connectivity matrix.
 #' @param z A vector of time-series data [n x 1]
-#' @param connect The connectivity matrix for the z vector [n x 2]. Each row contains last and next element of the time-series. NA values means not connected.
+#' @param connect The connectivity matrix for the z vector [n x 2]. Each row contains the last and next elements of the time-series. NA values means not connected.
 #' @param add A logical variable whether the plot should be overlaid on the current panel.
-#' @param col The color variable 
+#' @param col The color variable
 #' @param ylim Range of the y axis
 #' @param pch pch value for the points
 #' @param lwd lwd value for line tickness
@@ -22,16 +22,16 @@
 #'                   miss = 0.1, #fraction of missing data
 #'                   ymax = c(9,5,7, 3) #maximum of saturation trajectory
 #' )
-#' 
-#' ssOut <- fitCDM(x = ssSim$x, #predictors  
+#'
+#' ssOut <- fitCDM(x = ssSim$x, #predictors
 #'                 nGibbs = 2000,
 #'                 nBurnin = 1000,
 #'                 z = ssSim$z,#response
 #'                 connect = ssSim$connect, #connectivity of time data
 #'                 quiet=T)
-#' 
+#'
 #' summ <- getGibbsSummary(ssOut, burnin = 1000, sigmaPerSeason = F)
-#' 
+#'
 #' colMeans(summ$ymax)
 #' colMeans(summ$betas)
 #' colMeans(summ$tau)
@@ -45,5 +45,5 @@ phenoSimPlot <- function(z, connect, add=F, col='blue', ylim = range(z, na.rm = 
   ww2 <- which(is.na(connect[,2]))
   for(i in 1:length(ww1))lines(ix[ww1[i]:ww2[i]], z[ww1[i]:ww2[i]], col=col, lwd=lwd)
   abline(v=ix[ww1], col='grey')
-  
+
 }
