1d7e14eda76f1899569d132812fd97d65eb6ecd5
bnasr <bijan.s.nasr@gmail.com>
2018-05-01 16:36:19 -0700

    vignette is in

--- build.R
+++ build.R
@@ -4,18 +4,20 @@ if(!require("roxygen2")) {devtools::install_github("klutometis/roxygen"); librar
 
 PACKAGE.NAME = 'phenoCDM'
 # create('~/Projects/phenoCDM/')
+# use_vignette("getting-started")
 document(pkg = '.')
 install(pkg = '.')
 
 system('rm -r toCRAN')
 system('mkdir toCRAN')
-system(paste0('rm ', PACKAGE.NAME, '*.tar.gz'))
-system('cp -r R man DESCRIPTION NAMESPACE LICENSE inst toCRAN')
+system(paste0('rm ', PACKAGE.NAME, '*.tar.gz'), ignore.stderr = TRUE)
+system('cp -r R man vignettes DESCRIPTION NAMESPACE LICENSE inst toCRAN')
 f <- build('toCRAN')
 
-system(command = paste0('R CMD check --as-cran ', basename(f)))
-devtools::check('toCRAN')
 # devtools::revdep_check('toCRAN')
+devtools::check('toCRAN')
+system(command = paste0('R CMD check ', basename(f)))
+system(command = paste0('R CMD check --as-cran ', basename(f)))
 
 # system('rm -r toCRAN')
 # devtools::submit_cran('toCRAN')
